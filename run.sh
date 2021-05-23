9e93d45772007718b0bd090ce13231a99d42ca9e
okanmenevseoglu <okanmenevseoglu@gmail.com>
2021-05-24 01:55:01 +0200

    Added docker-compose support for the project.

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# To ease the build process Docker images are running the build process inside the containers. Thus, only a simple
+# docker-compose command should be sufficient.
+
+docker-compose up -d
